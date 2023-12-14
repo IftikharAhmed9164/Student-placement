6da71002aa19f2846f204d25cbf7b05da33fe97d
IftikharAhmed9164 <136202617+IftikharAhmed9164@users.noreply.github.com>
2023-12-14 02:14:54 -0800

    Delete Old Files

--- app/src/main/java/com/example/student_placement_application/AppCompatActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.student_placement_application;
-
-import android.os.Bundle;
-
-public class AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
-    }
-}
